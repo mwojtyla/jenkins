0d84f22954588df1fd774fba033916078158ff06
Martyna Wojtyla <martyna.wojtyla@bytamic.com>
2023-11-24 14:56:37 +0100

    Added classes SpringbootJenkinsApplication and SpringbootJenkinsApplicationTests

---
+++ src/main/java/jenkins/springbootjenkins/SpringbootJenkinsApplication.java
@@ -0,0 +1,20 @@
+package jenkins.springbootjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJenkinsApplication.class, args);}
+
+		@GetMapping
+		public String get () {
+			return "Hello Jenkins!";
+		}
+
+}
